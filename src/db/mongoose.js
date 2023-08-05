510ae7d027ef4ff7fb4460bf4437a0a65ab5c88e
Salikhovakbar <pcfjkjhkk@gmail.com>
2023-08-05 10:27:31 +0500

    moongose link changed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 (async () => {
 try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/nextgen')
+    await mongoose.connect('mongodb+srv://akbar:ngd7T3b5nkHoSYUb@cluster0.ie7lbnc.mongodb.net/nextgen?retryWrites=true&w=majority')
     console.log("The database has been connected")
 } catch (err) {
     console.log(err.message)
